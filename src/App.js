6fa4173350aa0189f370693d62b7fb957282ad45
Sergio Molano <sa.molano@uniandes.edu.co>
2023-11-23 12:55:37 -0500

    Front-End guessing number game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  // Variables de estado
+  const [responseText, setResponseText] = useState('');
+  const [attemptsText, setAttemptsText] = useState('');
+
+  // Manejo de click
+  const checkGuess = () => {
+    const userValue = Number(document.getElementById('guess').value);
+    const baseUrl = 'http://localhost:8080/adivinar/';
+    const guessUrl = `${baseUrl}${userValue}`;
+
+    // Fetch request 
+    fetch(guessUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(data => {
+        setResponseText(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+
+    const baseUrlIntentos = 'http://localhost:8080/intentos';
+
+    // Fetch request 
+    fetch(baseUrlIntentos)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(data => {
+        const attemptsText = `El número de intentos realizados es: ${data}`;
+        setAttemptsText(attemptsText);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  // Manejo de click
+  const newGame = () => {
+    const nueGameUrl = 'http://localhost:8080/nuevo';
+
+    // Fetch request 
+    fetch(nueGameUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(data => {
+        setResponseText("");
+        setAttemptsText("");
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Juego de Adivinanza de Números</h1>
+      <p>Adivina un número entre 1 y 100</p>
+      <input type="number" id="guess" min="1" max="100" />
+      <button id="submit" onClick={checkGuess}>
+        Enviar
+      </button>
+      <p id="hint">{responseText}</p>
+      <p id="attempts">{attemptsText}</p>
+      <button id="nuevo" onClick={newGame}>
+        Nuevo Juego
+      </button>
     </div>
   );
-}
+};
 
 export default App;
